d5feaa3eb410d1446096bdaf7efd74f3e4c4c948
KyungJae Lee <58136218+philia-lee@users.noreply.github.com>
2026-04-11 21:13:37 +0900

    Develop (#4)
    * product
    
    * change quantitiy
    
    * add prometaus & grafana

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
     implementation ("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test:4.0.3")
     testImplementation("com.h2database:h2")
